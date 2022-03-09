0603bc8cdd32099afbf3fc153363b32ff271e821
bezunkj.04 <bezunkj.04@A321-10>
2022-03-09 10:28:49 +0100

    Přidejte soubory projektu.

---
+++ Metody04/CelaCisla.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody04
+{
+    class CelaCisla
+    {
+        public int Mocnina(int zaklad, int exponent)
+        {
+            if(exponent < 0)
+            {
+                exponent *= -1;
+                zaklad = 1 / zaklad;
+            }
+
+            int vysledek = 1;
+            for(int i = 0; i < exponent; i++)
+            {
+                vysledek *= zaklad;
+            }
+
+            return vysledek;
+        }
+    }
+}
